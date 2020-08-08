a43304e456fa17b09a129865516a3c76ea9b07ed
Kristofer Tengström <info@kristofer-tengstrom.se>
2020-08-09 00:26:46 +0200

    Track src subfolders in git

---
+++ src/Misc/functions.php
@@ -0,0 +1,41 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Ordermind\Helpers\Misc;
+
+use Generator;
+use LogicException;
+
+/**
+ * Memory-efficient version of the native range() function.
+ *
+ * @param int $start
+ * @param int $end
+ * @param int $step  the step parameter is sanitized, so it doesn't matter if you provide a positive or negative number
+ *                   as long as it is not zero
+ *
+ * @return Generator
+ *
+ * @throws LogicException
+ */
+function xrange(int $start, int $end, int $step = 1): Generator
+{
+    if ($step == 0) {
+        throw new LogicException('Step cannot be zero');
+    }
+
+    $step = abs($step);
+
+    return (function ($start, $end, $step): Generator {
+        if ($start <= $end) {
+            for ($index = $start; $index <= $end; $index += $step) {
+                yield $index;
+            }
+        } else {
+            for ($index = $start; $index >= $end; $index -= $step) {
+                yield $index;
+            }
+        }
+    })($start, $end, $step);
+}
